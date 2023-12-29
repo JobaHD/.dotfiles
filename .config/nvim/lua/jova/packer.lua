978dfcb33bb85e9330e32240923436d3d2548b17
JobaHD <92954275+JobaHD@users.noreply.github.com>
2023-12-28 23:09:27 -0500

    changed the terminal and nvim colors to catpuccin

--- .config/nvim/lua/jova/packer.lua
+++ .config/nvim/lua/jova/packer.lua
@@ -13,9 +13,7 @@ return require('packer').startup(function(use)
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
-    use({
-        'folke/tokyonight.nvim',
-    })
+    use { "catppuccin/nvim", as = "catppuccin" }
 
     use({
         "folke/trouble.nvim",
--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -79,6 +79,11 @@ _G.packer_plugins = {
     path = "/home/jrod/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
+  catppuccin = {
+    loaded = true,
+    path = "/home/jrod/.local/share/nvim/site/pack/packer/start/catppuccin",
+    url = "https://github.com/catppuccin/nvim"
+  },
   ["cmp-buffer"] = {
     loaded = true,
     path = "/home/jrod/.local/share/nvim/site/pack/packer/start/cmp-buffer",
@@ -173,11 +178,6 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/jrod/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
-  },
-  ["tokyonight.nvim"] = {
-    loaded = true,
-    path = "/home/jrod/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
-    url = "https://github.com/folke/tokyonight.nvim"
   },
   ["trouble.nvim"] = {
     config = { "\27LJ\2\nC\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\1\nicons\1\nsetup\ftrouble\frequire\0" },
